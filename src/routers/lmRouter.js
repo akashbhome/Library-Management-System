965c68ba61a2bdf43983353ef6a0bfeed0622ba2
akashbhome <akashbhome1234@gmail.com>
2025-06-11 17:50:50 +0530

    About page add

--- src/routers/lmRouter.js
+++ src/routers/lmRouter.js
@@ -5,9 +5,10 @@ let router=routers.Router();
 router.get("/",controller.homepage);
 router.get("/login",controller.adminLogin);
 router.post("/adminLog",controller.admindash);
-<<<<<<< HEAD
-module.exports=router;
-=======
 router.get("/about",controller.about);
 module.exports=router;  
->>>>>>> 878b701f910d1a8ca11cc12ea1b26c557323fe79
+
+
+
+
+
